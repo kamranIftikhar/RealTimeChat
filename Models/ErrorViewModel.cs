9e1df3d88c04eb8634a770098d2b1bed00fb6879
kamran-Techsurge <kamran.iftikhar@techsurgeinc.com>
2024-09-11 10:23:53 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealTimeChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
